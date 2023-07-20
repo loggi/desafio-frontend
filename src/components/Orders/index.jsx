e113e197c860d3fbecebd3640d8513b860cd2fcb
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-07-20 20:05:16 -0300

    feat: Create order history feature

--- src/components/Orders/index.jsx
+++ src/components/Orders/index.jsx
@@ -2,8 +2,9 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import getOrdersTrack from '../../API/orders';
 import Header from '../Header';
-import {BoxDelivery, BoxStatus} from '../BoxInfos';
-import { pStyles } from './styles';
+import { BoxDelivery, BoxStatus } from '../BoxInfos';
+import { pStyles, orderHistory, styleStatus} from './styles';
+import StatusImage from '../StatusImage';
 
 function Orders() {
   const { trackingCode } = useParams();
@@ -25,23 +26,13 @@ function Orders() {
     return <div>Loading...</div>;
   }
 
-  const getStatusImage = (status) => {
-    const statusToImageMap = {
-      PEDIDO_RECEBIDO: "entregue.png",
-      EM_ROTA_PARA_COLETA: "enviado.png",
-      PACOTE_COLETADO: "pedido.png",
-    };
-    const imageName = statusToImageMap[status];
-    return `./assets/${imageName}`;
-  };
-
   return (
     <>
       <Header img src="./src/assets/logo.png" alt="logo loggi" style={{ height: '80px', marginRight: '10px' }} />
       <div>
         <div style={pStyles}>
-        <span> Obaaa!</span>
-        <span>Seu pedido está em transporte!</span>
+          <span> Obaaa!</span>
+          <span>Seu pedido está em transporte!</span>
         </div>
         <BoxDelivery
           messenger={order.mensageiro}
@@ -49,18 +40,18 @@ function Orders() {
           orderDate={order.dataPedido}
           deliveryDate={order.dataPrevistaEntrega}
         />
-        <BoxStatus 
-        orderNumber={order.id}
-        status={order.descricao}/>
-        <h2>Histórico de Status</h2>
+        <BoxStatus
+          orderNumber={order.id}
+          status={order.descricao} />
         <ul>
-          {order.historicoStatus.map((statusItem) => (
-            <li key={statusItem.status}>
-              <p>Status: {statusItem.status}</p>
-              <p>Descrição: {statusItem.descricao}</p>
-              <p>Data do status: {statusItem.dataStatus}</p>
-              <img src={getStatusImage(statusItem.status)} alt={statusItem.status} />
-            </li>
+          {order.historicoStatus.map((statusItem, index) => (
+            <section key={statusItem.status} style={orderHistory}>
+              <StatusImage status={statusItem.status} isLast={index === order.historicoStatus.length - 1} />
+              <div style={styleStatus}>
+                <span>{statusItem.descricao}</span>
+                <span>Data: {statusItem.dataStatus}</span>
+              </div>
+            </section>
           ))}
         </ul>
       </div>
