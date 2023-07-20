a125e0680238e05e3ff34b3c0a11be5ea1e93398
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-07-20 18:39:07 -0300

    add: styling background of the tracking page

--- pages/track/[pedidoId].js
+++ pages/track/[pedidoId].js
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { Accordion, AccordionDetails, AccordionSummary, Box, Container, Divider, Grid, List, ListItemText, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Container, Divider, Grid, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import { trackOrder } from "../../src/api/orderApi";
 import { ExpandCircleDown } from "@mui/icons-material";
 import Header from "../../src/components/Header";
+import { styled } from '@mui/material/styles';
+
+
+const StyledContainer = styled('div')({
+    backgroundColor: '#D9D9D9',
+    borderTopRightRadius: '10%',
+    borderBottomRightRadius: '70%',
+    borderBottomLeftRadius: '18%',
+    borderBottom: '30px solid #002766'
+
+});
 
 
 export default function Track() {
     const router = useRouter();
     const { pedidoId } = router.query;
     const [pedidoData, setPedidoData] = useState(null);
+
 
     useEffect(() => {
         if (pedidoId) {
@@ -21,100 +33,102 @@ export default function Track() {
 
     return (
         <>
-            <Header />
+            <StyledContainer>
+                <Header />
 
-            <Container
-                sx={{
+                <Container
+                    sx={{
 
-                    display: 'flex',
-                    justifyContent: 'center',
+                        display: 'flex',
+                        justifyContent: 'center',
 
-                    marginTop: '3rem'
-                }}>
+                        marginTop: '3rem'
+                    }}>
 
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                        {pedidoData ? (
-                            <Box sx={{
-                                alignItems: 'center',
-                                justifyContent: 'center'
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            {pedidoData ? (
+                                <Box sx={{
+                                    alignItems: 'center',
+                                    justifyContent: 'center'
 
-                            }}>
-                                <Typography variant="h5" textAlign="center">
-                                    Pedido: {pedidoId}
+                                }}>
+                                    <Typography variant="h5" textAlign="center">
+                                        PEDIDO: {pedidoId}
+                                    </Typography>
+                                    <Typography variant="h5" component="p" gutterBottom >
+
+                                        <pre>Mensageiro: {pedidoData.mensageiro}</pre>
+                                        <Divider />
+                                        <pre>Solicitado por: {pedidoData.cliente}</pre>
+                                        <Divider />
+                                        <pre>Data de registro: {pedidoData.dataPedido}</pre>
+                                        <Divider />
+                                        <pre>Status atual: {pedidoData.status}</pre>
+                                    </Typography>
+                                    <Accordion>
+                                        <AccordionSummary expandIcon={<ExpandCircleDown />}
+                                            aria-controls="panel1a-content"
+                                            id="panel1a-header"
+                                            sx={{ background: '#006AF0', color: 'white' }}>
+                                            <Typography variant="h4" component="h1" gutterBottom>
+                                                Endereço de entrega
+                                            </Typography>
+
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <Typography variant="p" gutterBottom>
+                                                Logradouro: {pedidoData.localEntrega.logradouro},
+                                                número: {pedidoData.localEntrega.numero},
+                                                complemento: {pedidoData.localEntrega.complemento},
+                                                bairro: {pedidoData.localEntrega.bairro}
+                                            </Typography>
+                                            <Typography variant="p" component="h5" gutterBottom>
+                                                {pedidoData.localEntrega.cidade} / {pedidoData.localEntrega.estado} - CEP: {pedidoData.localEntrega.cep}
+                                            </Typography>
+                                        </AccordionDetails>
+                                    </Accordion>
+                                    <Accordion>
+                                        <AccordionSummary expandIcon={<ExpandCircleDown />}
+                                            aria-controls="panel1a-content"
+                                            id="panel1a-header"
+                                            sx={{ background: '#006AF0', color: 'white' }}>
+                                            <Typography variant="h4" component="h1" gutterBottom>
+                                                Endereço de origem
+                                            </Typography>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <Typography variant="p" gutterBottom>
+                                                Logradouro: {pedidoData.localOrigem.logradouro},
+                                                número: {pedidoData.localOrigem.numero},
+                                                complemento: {pedidoData.localOrigem.complemento},
+                                                bairro: {pedidoData.localOrigem.bairro}
+                                            </Typography>
+                                            <Typography variant="p" component="h5" gutterBottom>
+                                                {pedidoData.localOrigem.cidade} / {pedidoData.localOrigem.estado} - CEP: {pedidoData.localOrigem.cep}
+                                            </Typography>
+                                        </AccordionDetails>
+                                    </Accordion>
+                                    <Accordion>
+                                        <AccordionSummary expandIcon={<ExpandCircleDown />}
+                                            aria-controls="panel1a-content"
+                                            id="panel1a-header"
+                                            sx={{ background: '#006AF0', color: 'white' }}>
+                                            <Typography variant="h4" component="h1">
+                                                Histórico do status
+                                            </Typography>
+                                        </AccordionSummary>
+                                    </Accordion>
+                                </Box>
+                            ) : (
+                                <Typography variant="h1" component="h1" gutterBottom >
+                                    ....
                                 </Typography>
-                                <Typography variant="h5" component="p" gutterBottom >
-
-                                    <pre>Mensageiro: {pedidoData.mensageiro}</pre>
-                                    <Divider />
-                                    <pre>Solicitado por: {pedidoData.cliente}</pre>
-                                    <Divider />
-                                    <pre>Data de registro: {pedidoData.dataPedido}</pre>
-                                    <Divider />
-                                    <pre>Status atual: {pedidoData.status}</pre>
-                                </Typography>
-                                <Accordion>
-                                    <AccordionSummary expandIcon={<ExpandCircleDown />}
-                                        aria-controls="panel1a-content"
-                                        id="panel1a-header"
-                                        sx={{ background: '#006AF0', color: 'white' }}>
-                                        <Typography variant="h4" component="h1" gutterBottom>
-                                            Endereço de Entrega
-                                        </Typography>
-
-                                    </AccordionSummary>
-                                    <AccordionDetails>
-                                        <Typography variant="p" gutterBottom>
-                                            Logradouro: {pedidoData.localEntrega.logradouro},
-                                            número: {pedidoData.localEntrega.numero},
-                                            complemento: {pedidoData.localEntrega.complemento},
-                                            bairro: {pedidoData.localEntrega.bairro}
-                                        </Typography>
-                                        <Typography variant="p" component="h5" gutterBottom>
-                                            {pedidoData.localEntrega.cidade} / {pedidoData.localEntrega.estado} - CEP: {pedidoData.localEntrega.cep}
-                                        </Typography>
-                                    </AccordionDetails>
-                                </Accordion>
-                                <Accordion>
-                                    <AccordionSummary expandIcon={<ExpandCircleDown />}
-                                        aria-controls="panel1a-content"
-                                        id="panel1a-header"
-                                        sx={{ background: '#006AF0', color: 'white' }}>
-                                        <Typography variant="h4" component="h1" gutterBottom>
-                                            Endereço de origem
-                                        </Typography>
-                                    </AccordionSummary>
-                                    <AccordionDetails>
-                                        <Typography variant="p" gutterBottom>
-                                            Logradouro: {pedidoData.localOrigem.logradouro},
-                                            número: {pedidoData.localOrigem.numero},
-                                            complemento: {pedidoData.localOrigem.complemento},
-                                            bairro: {pedidoData.localOrigem.bairro}
-                                        </Typography>
-                                        <Typography variant="p" component="h5" gutterBottom>
-                                            {pedidoData.localOrigem.cidade} / {pedidoData.localOrigem.estado} - CEP: {pedidoData.localOrigem.cep}
-                                        </Typography>
-                                    </AccordionDetails>
-                                </Accordion>
-                                <Accordion>
-                                    <AccordionSummary expandIcon={<ExpandCircleDown />}
-                                        aria-controls="panel1a-content"
-                                        id="panel1a-header"
-                                        sx={{ background: '#006AF0', color: 'white' }}>
-                                        <Typography variant="h4" component="h1">
-                                            Histórico do status
-                                        </Typography>
-                                    </AccordionSummary>
-                                </Accordion>
-                            </Box>
-                        ) : (
-                            <Typography variant="h1" component="h1" gutterBottom >
-                                ....
-                            </Typography>
-                        )}
+                            )}
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Container>
+                </Container>
+            </StyledContainer>
         </>
     );
 }
