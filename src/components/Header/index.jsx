f57acd8b0faca280f8b710c7a5754d85c6b5a471
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-07-20 03:47:05 -0300

    refactor(css): add styles to a new file

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,26 +2,15 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
 import ButtonConfirm from '../Buttom';
+import { listItemStyles, listAndLogoStyles } from './styles';
 
 
 function Header() {
-    const listItemStyles = {
-        marginRight: '20px',
-        cursor: 'pointer',
-        fontSize: '16px',
-        fontFamily: 'Arial, sans-serif',
-    };
-
-    const listAndLogoPosition = {
-        display: 'flex',
-        alignItems: 'center',
-    }
-
     return (
         <AppBar position="static" style={{ backgroundColor: 'var(--black)'}}>
             <Toolbar>
                 <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexGrow: 1 }}>
-                    <div style={listAndLogoPosition}>
+                    <div style={listAndLogoStyles}>
                         <img src="./src/assets/logo.png" alt="logo loggi" style={{ height: '80px', marginRight: '10px' }} />
                         <nav>
                             <ul style={{ display: 'flex', gap: '1rem', listStyleType: 'none', padding: 0, margin: 0 }}>
