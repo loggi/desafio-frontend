8f97eda96d0f0e54ea22ca2ac5da59ef62c6dbaf
Ana <ana_leticia@geb.inatel.br>
2023-07-19 23:38:36 -0300

    form addition and styling, header tweaks

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { ContainerHeader, Logo } from "./Styles";
+import { ContainerHeader, Logo, Paragrafo, Button, BlueText} from "./Styles";
 import LogoIcon from "../../Img/LogoLoggi.png";
 
 const Header = () => {
@@ -6,7 +6,12 @@ const Header = () => {
     return(
         <ContainerHeader>
             <Logo src={LogoIcon}></Logo>
-            
+            <Paragrafo>Para você</Paragrafo>
+            <Paragrafo>Para Empresas</Paragrafo>
+            <Paragrafo>Vendas e suporte</Paragrafo>
+            <BlueText>Acompanhe seu pedido</BlueText>
+            <Button>Entrar</Button>
+            <Button>Criar Conta</Button>
         </ContainerHeader>
     );
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,18 @@
-import { ContainerHome, Text, DivForm, ImgLogo, ContainerLeft, ContainerRight, DivText, DivImg } from "./Styles";
+import { 
+    ContainerHome, 
+    FormContainer, 
+    Title, 
+    Input,
+    IntroText, 
+    Form, 
+    SubmitButton,
+    ContainerRight,
+    ContainerLeft, 
+    ImgLogo, 
+    BlueText, 
+    DivText, 
+    Text,
+    DivImg } from "./Styles";
 import IconRabbit from "../../Img/LogoLoggi2.png"
 
 const Home= () => {
@@ -9,7 +23,7 @@ const Home= () => {
                 <DivText>
                     <Text>VOCÊ E SUA</Text>
                     <Text>EMPRESA PODEM</Text>
-                    <Text>FAZER ENVIOS PARA</Text>
+                    <Text>FAZER <BlueText>ENVIOS</BlueText> PARA</Text>
                     <Text>TODO O BRASIL</Text>
                 </DivText>
                 <DivImg>
@@ -17,9 +31,20 @@ const Home= () => {
                 </DivImg>
             </ContainerLeft>
             <ContainerRight>
-                <DivForm>
-
-                </DivForm>
+                <FormContainer>
+                    <Title>ACOMPANHE SEU PEDIDO</Title>
+                    
+                    <IntroText>PRIMEIRO, DIGITE SEU </IntroText>
+                    <IntroText>SEU CÓDIGO DE RASTREAMENTO</IntroText>
+                    
+                    <Form>
+                        <Input
+                        type="text"
+                        placeholder="Digite o código de rastreio"
+                        />
+                        <SubmitButton type="submit">RASTREAR</SubmitButton>
+                    </Form>
+                </FormContainer>
             </ContainerRight>
         </ContainerHome>
     );
