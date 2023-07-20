efe2879a9a16012ed06ec9024ad2efb7b9d27cda
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-07-20 13:41:13 -0300

    add: create component ImageLogo

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 import * as React from 'react';
 import FormBox from '../src/components/FormBox';
 import Header from '../src/components/Header';
+import { Container, Grid, Box } from '@mui/material';
+import ImageHome from '../src/components/Images/ImageHome';
 
 export default function Index() {
   return (<>
     <Header />
-    <FormBox />
+    <Container maxWidth="lg">
+      <Grid container spacing={3}>
+
+        <Grid item xs={12} sm={6} sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          <FormBox />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <ImageHome />
+        </Grid>
+      </Grid>
+    </Container>
   </>
 
   );
---
+++ src/components/Images/ImageHome.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const ImageHome = () => {
+    return (
+        <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            height="100%"
+        >
+            <img
+                src={'https://github.com/paola-oliveira/desafio-frontend/blob/main/src/Imagens/iconHome.png?raw=true'}
+                alt="Logo Loggi"
+                style={{
+                    maxWidth: '100%',
+                    height: 'auto',
+                    width: '100%',
+                    objectFit: 'contain',
+                }}
+            />
+        </Box>
+    );
+};
+
+export default ImageHome;
