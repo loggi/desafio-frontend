18e8b49b66295e3901fd09f7ecaff3769e041fc5
bruna-abreu <bruna.abreu95@gmail.com>
2023-07-20 17:06:11 -0300

    feat: request API data, create routes and click event

---
+++ src/Page/TrackOrder.jsx
@@ -0,0 +1,9 @@
+import GetOrders from "../Components/Orders/GetOrders";
+
+function TrackOrder(){
+    return (
+        <GetOrders/>
+    )
+}
+
+export default TrackOrder;
---
+++ src/Route.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import TrackingPage from './Page/TrackingPage'
+import TrackOrder from './Page/TrackOrder'
+
+
+ function TrackerRoute() {
+  return (
+        <BrowserRouter>        
+            <Routes>
+                <Route path="/" element={<TrackingPage />} />
+                <Route path="/TrackOrder/:trackingCode" element={<TrackOrder/>} />            
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default TrackerRoute;
