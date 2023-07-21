0f303a007e6157f3a3b9dddf2b956cae4b6032c2
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-07-21 00:19:40 -0300

    add: responsiveness in the ImageLogo component

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,35 @@ import * as React from 'react';
 import FormBox from '../src/components/FormBox';
 import { Container, Grid, Box } from '@mui/material';
 import ImageHome from '../src/components/Images/ImageHome';
-import Copyright from '../src/Copyright';
+import { useMediaQuery } from '@mui/material';
 
 export default function Index() {
-  return (<>
-    <Container maxWidth="lg" sx={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: '3rem',
-    }}>
-      <Grid container spacing={2}>
+  const isScreenSmall = useMediaQuery((theme) => theme.breakpoints.down('sm'));
 
-        <Grid item xs={12} sm={6}>
-          <ImageHome />
+  return (
+    <>
+      <Container maxWidth="lg" sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '3rem',
+      }}>
+        <Grid container spacing={2}>
+          {!isScreenSmall && (
+            <Grid item xs={12} sm={6}>
+              <ImageHome />
+            </Grid>
+          )}
+
+          <Grid item xs={12} sm={6} sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <FormBox />
+          </Grid>
         </Grid>
-
-        <Grid item xs={12} sm={6} sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
-          <FormBox />
-        </Grid>
-      </Grid>
-    </Container>
-  </>
-
+      </Container>
+    </>
   );
 }
