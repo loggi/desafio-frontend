9f602bccc6877bff02ddd45910171a7c7d2db9c0
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-07-20 22:01:51 -0300

    fix: header component responsiveness for different screen sizes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,75 @@
-import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box, Button, IconButton, Menu, MenuItem } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import Logo from './Logo';
-
+import { useState } from 'react';
 
 const Header = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const isMenuOpen = Boolean(anchorEl);
+
+    const menuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const menuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const menuId = 'header-menu';
+
     return (
         <AppBar position="static">
             <Toolbar>
                 <Logo />
                 <Box display="flex">
-                    <Typography variant="subtitle1" sx={{ mr: 3, color: 'black', cursor: 'pointer' }}>
-                        Para você
-                    </Typography>
-                    <Typography variant="subtitle1" sx={{ mr: 3, color: 'black', cursor: 'pointer' }}>
-                        Para empresas
-                    </Typography>
-                    <Typography variant="subtitle1" sx={{ mr: 3, color: 'black', cursor: 'pointer' }}>
-                        Para entregar
-                    </Typography>
-                    <Typography variant="subtitle1" sx={{ mr: 3, color: 'black', cursor: 'pointer' }}>
-                        Vendas e Suporte
-                    </Typography>
+
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={menuOpen}
+                        sx={{
+                            display: { xs: 'block', sm: 'none', color: 'black' },
+                            justifyContent: 'center',
+                            alignItens: 'center',
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Box display={{ xs: 'none', sm: 'flex' }}>
+                        <Typography variant="subtitle1" sx={{ mr: 3, color: 'black', cursor: 'pointer' }}>
+                            Para você
+                        </Typography>
+                        <Typography variant="subtitle1" sx={{ mr: 3, color: 'black', cursor: 'pointer' }}>
+                            Para empresas
+                        </Typography>
+                        <Typography variant="subtitle1" sx={{ mr: 3, color: 'black', cursor: 'pointer' }}>
+                            Para entregar
+                        </Typography>
+                        <Typography variant="subtitle1" sx={{ mr: 3, color: 'black', cursor: 'pointer' }}>
+                            Vendas e Suporte
+                        </Typography>
+                    </Box>
                 </Box>
+
                 <Box display="flex" alignItems="center" flexGrow={2} justifyContent="flex-end">
-                    <Button sx={{
-                        color: 'blue',
-                        border: '2px solid #006AF0'
-                    }}>
-                        Entrar
-                    </Button>
+                    <Button sx={{ color: 'blue', border: '2px solid #006AF0' }}>Entrar</Button>
                 </Box>
+                <Menu
+                    anchorEl={anchorEl}
+                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                    id={menuId}
+                    keepMounted
+                    transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                    open={isMenuOpen}
+                    onClose={menuClose}
+                    sx={{ display: { xs: 'block', sm: 'none' } }}
+                >
+                    <MenuItem onClick={menuClose}>Para você</MenuItem>
+                    <MenuItem onClick={menuClose}>Para empresas</MenuItem>
+                    <MenuItem onClick={menuClose}>Para entregar</MenuItem>
+                    <MenuItem onClick={menuClose}>Vendas e Suporte</MenuItem>
+                </Menu>
             </Toolbar>
         </AppBar>
     );
