e588fe6d11825f37593cf0f5f7f0b49506d354a4
caiorobertosilva <caiorobertodasilva@gmail.com>
2023-10-21 23:40:56 -0300

    added simple form validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
+'use client'
+
 import Image from 'next/image'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { Button, TextField } from '@mui/material';
 
 export default function Home() {
+  const reg = new RegExp("[0-9]");
+
+  const [codevalue, setCodeValue] = useState("");
+  const [valid, setValid] = useState(true);
+
+  const handleValidation = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setCodeValue(e.target.value);
+    setValid(reg.test(e.target.value));
+  };
+
+  const onClick = () => {
+    setValid(reg.test(codevalue) && codevalue.length > 0);
+  }
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+  }
+
   return (
     <>
       <header className="h-24 p-8 w-full">
-        <Image
-          alt="Loggi Logo"
-          height={ 34 }
-          priority
-          src="/loggi.svg"
-          width={ 100 }
-        />
+        <Image alt="Loggi Logo" height={ 34 } priority src="/loggi.svg" width={ 100 } />
       </header>
 
       <main className="flex flex-col">
@@ -23,11 +38,19 @@ export default function Home() {
               <span className="font-sans inline-block mt-4 text-base span--small">Primeiro, digite o código de rastreamento.</span>
               <span className="font-sans inline-block mt-6 text-base span--small">Código de rastreamento.</span>
 
-              <form>
-                <TextField id="outlined-basic" variant="outlined" className="mt-1 w-full" />
+              <form onSubmit={ onSubmit }>
+                <TextField
+                  className={"mt-1 w-full" + (!valid ? ' error' : '')}
+                  error={ !valid }
+                  id="outlined-basic"
+                  inputProps={{ pattern: "[0-9]" }}
+                  onChange={ (e) => handleValidation(e) }
+                  required={ true }
+                  value={ codevalue }
+                  variant="outlined" />
 
                 <div className="flex justify-center mt-10">
-                  <Button variant="contained" className="font-sans">Acompanhar pedido</Button>
+                  <Button variant="contained" className="font-sans" type="submit" onClick={ onClick }>Acompanhar pedido</Button>
                 </div>
               </form>
             </div>
@@ -35,12 +58,7 @@ export default function Home() {
 
           <section className="h-full w-1/2">
             <div className="h-full relative">
-              <Image
-                alt="Loggi Order"
-                className="object-contain img"
-                fill
-                src="/loggi_order.svg"
-              />
+              <Image alt="Loggi Order" className="object-contain img" fill priority src="/loggi_order.svg" />
             </div>
           </section>
         </div>
