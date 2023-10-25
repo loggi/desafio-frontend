af1ae1680f5d1871b08ebc6df0c9aec42a498b1a
csdg78 <csdg78@gmail.com>
2023-10-25 03:52:58 -0300

    add head tag + viewport + fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,17 +2,30 @@ import '../styles/global.scss'
 
 import styles from  '../styles/app.module.scss'
 import Header from '@/components/header'
+import Head from 'next/head'
 
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>  
-    </div>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+        />
+
+      </Head>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>  
+      </div>
+    </>    
   )
 }
 
