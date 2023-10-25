afa634cb05566ab011b3bed6ffd87c6ff6e6dbaf
csdg78 <csdg78@gmail.com>
2023-10-25 03:52:01 -0300

    header layout + navigation

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,41 @@
+import Image from 'next/image';
+
 import styles from './styles.module.scss';
+import Logo from './../../assets/logo-loggi.svg'
+
+import { Button } from '@mui/base/Button';
+import { Box } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
 
+    const [displayNav, setDisplayNav] = useState(false)
+
+    useEffect(() => {
+        setDisplayNav(window.innerWidth <= 768 ? false : true)        
+    }, [])
+
     return (
-        <header className={styles.header__container}>
-            header
-        </header>
+        <Box component="header" className={styles.header__container}>
+            <Image className={styles.navigation__logo} src={Logo} alt='Loggi logo'/>
+            <Box className={styles.hamburguer__container}>
+                <Button className={`${styles.hamburguer} ${displayNav ? styles.showNav : styles.hideNav}`} onClick={() => setDisplayNav(!displayNav)} />
+            </Box>
+            <Box className={`${styles.navigation__container} ${displayNav ? styles.showNav : styles.hideNav} `}>
+                <nav className={styles.navigation}>
+                    <ul>
+                        <li><a href=''>Para você</a></li>
+                        <li><a href=''>Para empresas</a></li>
+                        <li><a href=''>Para entregar</a></li>
+                        <li><a href=''>Vendas e suporte</a></li>
+                        <li><a href=''>Acompanhe seu pedido</a></li>
+                    </ul>                
+                </nav>
+                <Box className={styles.navigation__buttons} >
+                    <Button className={styles.button__login}>Entrar</Button>
+                    <Button className={styles.button__signup}>Criar conta</Button>
+                </Box>
+            </Box>            
+        </Box>        
     )
 }
