ce17ed8dbe6be2ef023ff98f74c3934029ac00f4
csdg78 <csdg78@gmail.com>
2023-10-26 08:58:24 -0300

    create component for status bar

--- src/pages/order/[id].js
+++ src/pages/order/[id].js
@@ -1,9 +1,4 @@
-import { Container, Box, Typography   } from '@mui/material';
-
-import TaskAltOutlinedIcon from '@mui/icons-material/TaskAltOutlined';
-import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
-import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
-import InventoryOutlinedIcon from '@mui/icons-material/InventoryOutlined';
+import { Container, Box  } from '@mui/material';
 
 import Head from 'next/head';
 import { useRouter } from 'next/router'
@@ -11,37 +6,7 @@ import { useEffect, useState } from 'react';
 
 import styles from './order.module.scss';
 import { trackOrder } from '@/api/order';
-
-export const trackingStatusImage = {
-  PEDIDO_RECEBIDO: 'Pedido recebido',
-  EM_ROTA_PARA_COLETA: 'Em rota para coleta',
-  PACOTE_COLETADO: 'Pacote coletado',
-  EM_ROTA_DE_ENTREGA: 'Em rota de entrega',
-  FINALIZADO: 'Finalizado'
-};
-
-export const trackingStatusSteps = [
-  {
-    step: 1,
-    description: 'Pedido recebido',
-  },
-  {
-    step: 2,
-    description: 'Em rota para coleta',
-  },
-  {
-    step: 3,
-    description: 'Pacote coletado',
-  },
-  {
-    step: 4,
-    description: 'Em rota de entrega',
-  },
-  {
-    step: 5,
-    description: 'Finalizado'
-  }
-]
+import TrackingSteps from '@/components/trackingSteps/trackingSteps';
 
 export default function Order() {
 
@@ -59,83 +24,20 @@ export default function Order() {
     if(trackingCode) {
       getOrder();
     }
-
   }, [trackingCode])
-
-  const getTrackingStatus = (status) => {
-    switch (status) {
-      case trackingStatusImage.PEDIDO_RECEBIDO:
-        return <AddShoppingCartOutlinedIcon />
-        break;
-      case trackingStatusImage.EM_ROTA_PARA_COLETA:
-        return <LocalShippingOutlinedIcon />
-        break;
-      case trackingStatusImage.PACOTE_COLETADO:
-        return <InventoryOutlinedIcon />
-        break;
-      case trackingStatusImage.EM_ROTA_DE_ENTREGA:
-        return <LocalShippingOutlinedIcon />
-        break;
-      case trackingStatusImage.FINALIZADO:
-        return <TaskAltOutlinedIcon />
-        break;
-      default:
-        return <LocalShippingOutlinedIcon />
-    }
-  }
 
   return (
     <div className={styles.tracker}>   
       <Head>
         <title>Acompanhe suas Entregas em Tempo Real | Loggi</title>
       </Head>
-      <Container sx={{ 
-          margin: '50px auto',
-          padding: '50px' 
-        }}
-        >
-        {order && order.historicoStatus && 
-          <ul className={styles.tracker__statusList}>
-          {order.historicoStatus.map((status, index) => {
-            let curretStatus = index + 1 === order.historicoStatus.length;
-             return (<li key={index} className={`${curretStatus ? styles.currentStatus : styles.status}`}>
-              <Box className={styles.trackerStatus__iconContainer}>
-                {getTrackingStatus(status.descricao)}
-              </Box>                
-              <Box className={styles.trackerStatus__infoContainer}>
-                <Typography variant="body1" gutterBottom color="#000">
-                  {status.descricao}
-                </Typography>
-                <Typography variant="body2" gutterBottom color="#999">
-                  {status.dataStatus}
-                </Typography>
-              </Box>              
-            </li>)
-          })}
-          {order.historicoStatus.length < 5 && 
-                        
-            trackingStatusSteps.map((step, i) => {
-              return (
-                step.step > order.historicoStatus.length ? 
-                  <li key={i} className={styles.trackerStatus__inactive}>
-                    <Box className={styles.trackerStatus__iconContainer}>
-                      {getTrackingStatus(step.description)}
-                    </Box>                
-                    <Box className={styles.trackerStatus__infoContainer}>
-                      <Typography variant="body1" gutterBottom color="#000">
-                        {step.description}
-                      </Typography>
-                      <Typography variant="body2" gutterBottom color="#999">
-                        sem data
-                      </Typography>
-                    </Box>                    
-                  </li> 
-                : ''
-              )
-            })
-          }
-        </ul>
-        }
+      <Container sx={{ margin: '50px auto', padding: '50px' }} >
+        <Box className={styles.order__info}>
+
+        </Box>
+        <Box className={styles.order__trackingSteps}>
+          <TrackingSteps order={order} />
+        </Box>        
       </Container>
     </div>
   )
