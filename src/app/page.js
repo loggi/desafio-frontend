cd6bca1fbe1426cbecc8d87471ec6775008e3498
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2023-10-27 19:30:31 -0300

    feat: adicionado elementos de layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,72 +1,26 @@
-import * as React from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
-import Drawer from '@mui/material/Drawer';
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-import MediaCard from '@/components/MediaCard';
+import TrackingForm from '../components/TrackingForm/TrackingForm';
 
 export default function HomePage() {
+
   return (
-    <Box sx={{ display: 'flex' }}>
-      <div>
-        <Alert severity="info" sx={{ mt: 2, mb: 5 }}>
-          <AlertTitle>Hello 👋</AlertTitle>
-          This app uses the Next.js App Router and Material UI v5.
-        </Alert>
-        <Grid container rowSpacing={3} columnSpacing={3}>
-          <Grid xs={6}>
-            <MediaCard
-              heading="CMYK"
-              text="The CMYK color model (also known as process color, or four color) is a subtractive color model, based on the CMY color model, used in color printing, and is also used to describe the printing process itself."
-            />
-          </Grid>
-          <Grid xs={6}>
-            <MediaCard
-              heading="HSL and HSV"
-              text="HSL (for hue, saturation, lightness) and HSV (for hue, saturation, value; also known as HSB, for hue, saturation, brightness) are alternative representations of the RGB color model, designed in the 1970s by computer graphics researchers."
-            />
-          </Grid>
-          <Grid xs={6}>
-            <MediaCard
-              heading="RGB"
-              text="An RGB color space is any additive color space based on the RGB color model. RGB color spaces are commonly found describing the input signal to display devices such as television screens and computer monitors."
-            />
-          </Grid>
-          <Grid xs={6}>
-            <MediaCard
-              heading="CIELAB"
-              text="The CIELAB color space, also referred to as L*a*b*, was intended as a perceptually uniform space, where a given numerical change corresponds to a similar perceived change in color."
-            />
-          </Grid>
-        </Grid>
-      </div>
-      <Drawer
-        sx={{
-          width: 320,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: 320,
-            boxSizing: 'border-box',
-            top: ['48px', '56px', '64px'],
-            height: 'auto',
-            bottom: 0,
-          },
-        }}
-        variant="permanent"
-        anchor="right"
-      >
-        <List sx={{ px: 2 }}>
-          <ListItem disablePadding>
-            <Typography variant="overline" sx={{ fontWeight: 500 }}>
-              On this page
-            </Typography>
-          </ListItem>
-        </List>
-      </Drawer>
+    <Box sx={{ 
+      display: 'flex', 
+      background: "url(https://s3-sa-east-1.amazonaws.com/loggi-production-static/tracker-web-ui/images/package-ff9eb72c01531c46f6a2c2a5fbcb975c.png) no-repeat 100% 100%,linear-gradient(180deg,#006aff,#00baff)",
+      backgroundSize: '550px,100vw',
+      height: '100%',
+    }}>
+      <Grid xs={6} sx={{
+        p: {
+          md: 14,
+          sm: 8,
+          xs: 6
+        }
+      }}>
+        <TrackingForm />
+      </Grid>
     </Box>
   );
 }
