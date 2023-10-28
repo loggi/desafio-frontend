3510096335bef10ca94caddf98fdf36bea46b62e
jonataspinto <jonatastdb_jp@hotmail.com>
2023-10-28 18:22:15 -0300

    chore(jest): configure module name mapper and create mock to window.resizeTo

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,11 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({ dir: "./" });
 
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["./jest.setup.js"],
   testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
 };
 
 module.exports = createJestConfig(customJestConfig);
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,27 @@
 import "@testing-library/jest-dom";
+
+Object.defineProperty(window, "resizeTo", {
+  value: (width, height) => {
+    Object.defineProperty(window, "innerWidth", {
+      configurable: true,
+      writable: true,
+      value: width,
+    });
+    Object.defineProperty(window, "outerWidth", {
+      configurable: true,
+      writable: true,
+      value: width,
+    });
+    Object.defineProperty(window, "innerHeight", {
+      configurable: true,
+      writable: true,
+      value: height,
+    });
+    Object.defineProperty(window, "outerHeight", {
+      configurable: true,
+      writable: true,
+      value: height,
+    });
+    window.dispatchEvent(new Event("resize"));
+  },
+});
