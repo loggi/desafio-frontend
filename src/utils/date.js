9eadb419e8e28c50e403e71ebfdca40b33b27534
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2023-10-28 14:55:19 -0300

    doc: adiciona jsdoc para func formatDate

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,18 @@
+
+/**
+ * Formata uma data EPOCH (em segundos) para uma string no formato 'DD/MM/AAAA'.
+ *
+ * @param {number} dateEta - A data EPOCH em segundos que deseja formatar.
+ * @returns {string} A data formatada no formato 'DD/MM/AAAA'.
+ *
+ * @example
+ * // Retorna '01/01/2023'
+ * formatDate(1672531200);
+ *
+ * @example
+ * // Retorna '15/11/2022'
+ * formatDate(1679126400);
+ */
 export const formatDate = (dateEta) => {
     const d = new Date(dateEta * 1000);
     const day = d.getDate() < 10 ? `0${d.getDate()}` : d.getDate();
