70d15572efa837f2660e33778d59da6787201d4f
jonataspinto <jonatastdb_jp@hotmail.com>
2023-10-29 01:26:59 -0300

    feat(app/orders): implements get order details route

---
+++ src/app/orders/[trackCode]/route.ts
@@ -0,0 +1,20 @@
+import orders from "@/_mock/orders.json";
+
+export async function GET(request: Request, { params }) {
+  const { trackCode } = params;
+
+  // fake delay
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+
+  const orderData = orders.find(
+    ({ trackingNumber }) => trackingNumber === trackCode
+  );
+
+  if (!orderData) {
+    return new Response("Order not found", { status: 404 });
+  }
+
+  return Response.json(orderData);
+}
