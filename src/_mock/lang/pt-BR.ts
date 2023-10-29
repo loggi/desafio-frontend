e76521f47a8d715f70c081c58622f1d5e7581fa8
jonataspinto <jonatastdb_jp@hotmail.com>
2023-10-28 21:35:16 -0300

    feat(_mock/lang): create mock with texts

---
+++ src/_mock/lang/pt-BR.ts
@@ -0,0 +1,24 @@
+export default {
+  metadata: {
+    title: "Loggi",
+    description:
+      "Com a Loggi você consegue acompanhar sua entrega e saber exatamente quando seu pedido vai chegar. Acesse nosso rastreador e veja como é fácil!",
+  },
+  trackerPage: {
+    metadata: {
+      title: "Acompanhe suas Entregas em Tempo Real | Loggi",
+    },
+    form: {
+      title: "Olá! Acompanhe com a Loggi a entrega do seu pedido.",
+      subTitle: "Primeiro, digite o código de rastreamento.",
+      inputPlaceholder: "Código de rastreamento",
+      buttonLabel: "Acompanhar pedido",
+    },
+    aside: {
+      title: "A melhor solução para envio de encomendas no Brasil.",
+      subTitle:
+        "Alcance mais clientes com entregas locais e nacionais. Com a Loggi, você economiza tempo e dinheiro.",
+      buttonLabel: "Enviar agora",
+    },
+  },
+};
