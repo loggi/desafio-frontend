c4eafb1bc21a6291b7af9403b81ba8e5aa7a507c
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2023-10-28 21:18:16 -0300

    chore: remove babel dos testes e next build

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,29 @@
-module.exports = {
-    roots: ["<rootDir>/src"],
-    moduleFileExtensions: ["js", "jsx", "json", "node"],
-    testPathIgnorePatterns: ["/node_modules/"],
-    testEnvironment: "jsdom",
-    transform: {
-      "^.+\\.(js|jsx)$": "babel-jest",
-    },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js|jsx)$",
-    moduleNameMapper: {
-      "^.+\\.(css|less|scss)$": "identity-obj-proxy",
-    },
-    collectCoverageFrom: ["src/**/*.{js,jsx}"],
-    coverageDirectory: "coverage",
-    coveragePathIgnorePatterns: ["src/index.js"],
-    coverageReporters: ["json", "lcov", "text", "clover"],
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-    },
-  };
-  
+const nextJest = require('next/jest')
+ 
+// Providing the path to your Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest({ dir: './' })
+ 
+// Any custom config you want to pass to Jest
+const customJestConfig = {
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+  testPathIgnorePatterns: ["/node_modules/"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js|jsx)$",
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["src/index.js"],
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
+ 
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
+module.exports = createJestConfig(customJestConfig)
