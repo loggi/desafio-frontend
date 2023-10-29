9f07d651202d5927c72cf7764266d1d76473db1c
jonataspinto <jonatastdb_jp@hotmail.com>
2023-10-29 01:21:15 -0300

    feat(components/TrackerForm): implements custom hook with validations and submit

--- src/components/TrackerForm/index.tsx
+++ src/components/TrackerForm/index.tsx
@@ -1,4 +1,3 @@
-import React, { SyntheticEvent, useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import FormControl from "@mui/material/FormControl";
@@ -6,7 +5,7 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
 import { useTheme } from "@mui/material";
-import { useRouter } from "next/navigation";
+import { useTrackerForm } from "./useTrackerForm";
 
 type TrackerFormProps = {
   title: string;
@@ -22,17 +21,7 @@ export function TrackerForm({
   buttonLabel,
 }: TrackerFormProps) {
   const { palette } = useTheme();
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
-
-  function handleSubmit(event: SyntheticEvent) {
-    setIsLoading(true);
-    event.preventDefault();
-    const { orderId } = event.target as typeof event.target & {
-      orderId: { value: string };
-    };
-    router.push(`/tracker/${orderId.value}`);
-  }
+  const { handleSubmit, handleChange, isLoading, error } = useTrackerForm();
 
   return (
     <Box
@@ -64,10 +53,13 @@ export function TrackerForm({
         <TextField
           label={inputPlaceholder}
           variant="outlined"
-          name="orderId"
+          name="trackCode"
           sx={{
             minWidth: { xs: "100%" },
           }}
+          error={!!error}
+          helperText={error || ""}
+          onChange={handleChange}
         />
       </FormControl>
       <Button
---
+++ src/components/TrackerForm/useTrackerForm.ts
@@ -0,0 +1,69 @@
+import { useRouter } from "next/navigation";
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+
+type UseTrackerForm = {
+  handleSubmit: (event: SyntheticEvent) => Promise<void>;
+  handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  clearError: () => void;
+  isLoading: boolean;
+  error?: string;
+};
+
+export function useTrackerForm(): UseTrackerForm {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  function clearError() {
+    setError(null);
+  }
+
+  function handleChange() {
+    if (error) {
+      clearError();
+    }
+  }
+
+  async function handleSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    const { trackCode } = event.target as typeof event.target & {
+      trackCode: { value: string };
+    };
+
+    if (!trackCode.value) {
+      setError("Digite um código válido");
+      return;
+    }
+
+    setIsLoading(true);
+
+    await fetch(`/orders/${trackCode.value}`)
+      .then(async (response) => {
+        if (response.status && response.status > 399) {
+          throw new Error(`${response.status} (${response.statusText})`);
+        }
+
+        const { orderNumber, trackingNumber } = await response.json();
+
+        router.push(`/tracker/${orderNumber}/${trackingNumber}`);
+      })
+      .catch((error) => {
+        if (error.message.includes(404)) {
+          setError("Não encontramos pedidos com esse código");
+        } else {
+          setError("Ocorreu um erro ao buscar o pedido");
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  return {
+    handleSubmit,
+    clearError,
+    handleChange,
+    isLoading,
+    error,
+  };
+}
