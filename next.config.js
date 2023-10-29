3345b08721c182156b14ab8be4d5fa6a2f649e31
jonataspinto <jonatastdb_jp@hotmail.com>
2023-10-29 14:26:09 -0300

    chore(next.config): configure rewrites for friendly routes

--- next.config.js
+++ next.config.js
@@ -7,6 +7,18 @@ const nextConfig = {
       transform: "@mui/icons-material/{{member}}",
     },
   },
+  async rewrites() {
+    return [
+      {
+        source: "/rastreamento",
+        destination: "/tracker",
+      },
+      {
+        source: "/rastreamento/:slug*",
+        destination: "/tracker/:slug*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
