1fb25d1424adad19bccc001b947603ea97981642
jonataspinto <jonatastdb_jp@hotmail.com>
2023-10-30 01:30:41 -0300

    feat(components/Footer): rendering logo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Divider from "@mui/material/Divider";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import lang from "@/_mock/lang/pt-BR";
 import { FooterMenu } from "./FooterMenu";
+import { Logo } from "../Logo";
 
 export function Footer() {
   const { footer } = lang;
@@ -15,10 +19,29 @@ export function Footer() {
             xs: 0,
             lg: 8,
           },
+          paddingBottom: {
+            xs: 2,
+            lg: 2,
+          },
         }}
       >
         <FooterMenu items={footer.footerMenuLinks} />
         <Divider />
+        <Button component={Link} href="/" sx={{ marginTop: 2 }} fullWidth>
+          <Typography variant="caption" textTransform="initial" marginLeft={1}>
+            {`© Loggi ${new Date().getFullYear()}`}
+          </Typography>
+          <Typography
+            variant="caption"
+            textTransform="initial"
+            marginLeft={1}
+            marginRight={1}
+            component="span"
+          >
+            |
+          </Typography>
+          <Logo ariaLabel="Logo Loggi" />
+        </Button>
       </Container>
     </Box>
   );
