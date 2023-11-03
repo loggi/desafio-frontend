398ec4574835385faf8142fc95cb49abd5b2371b
alalbux <alalbux@MacBook-Air-de-alalbux.local>
2023-11-03 17:21:35 -0300

    feat(core): add lint and prettier configs

--- docs/package-tracker/src/app/layout.js
+++ docs/package-tracker/src/app/layout.js
@@ -46,54 +46,15 @@ export default function RootLayout({ children }) {
         <ThemeRegistry>
           <AppBar position="fixed" sx={{ zIndex: 2000 }}>
             <Toolbar sx={{ backgroundColor: 'background.paper' }}>
-              <DashboardIcon sx={{ color: '#444', mr: 2, transform: 'translateY(-2px)' }} />
+              <DashboardIcon
+                sx={{ color: '#444', mr: 2, transform: 'translateY(-2px)' }}
+              />
               <Typography variant="h6" noWrap component="div" color="black">
-                Next.js App Router
+                Loggi
               </Typography>
             </Toolbar>
           </AppBar>
-          <Drawer
-            sx={{
-              width: DRAWER_WIDTH,
-              flexShrink: 0,
-              '& .MuiDrawer-paper': {
-                width: DRAWER_WIDTH,
-                boxSizing: 'border-box',
-                top: ['48px', '56px', '64px'],
-                height: 'auto',
-                bottom: 0,
-              },
-            }}
-            variant="permanent"
-            anchor="left"
-          >
-            <Divider />
-            <List>
-              {LINKS.map(({ text, href, icon: Icon }) => (
-                <ListItem key={href} disablePadding>
-                  <ListItemButton component={Link} href={href}>
-                    <ListItemIcon>
-                      <Icon />
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-            <Divider sx={{ mt: 'auto' }} />
-            <List>
-              {PLACEHOLDER_LINKS.map(({ text, icon: Icon }) => (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      <Icon />
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-          </Drawer>
+
           <Box
             component="main"
             sx={{
