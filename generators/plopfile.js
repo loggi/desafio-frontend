4e63e2621c3b6e6808baa32b2a047f998e23a302
André Beserra de Souza <andre.souza@cobasi.com.br>
2023-11-11 16:19:50 -0300

    style(plop): configuration for generate files

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,19 +1,78 @@
 module.exports = (plop) => {
-  plop.setGenerator('page', {
-    description: 'Create a page',
+  plop.setGenerator('component', {
+    description: 'Create a component',
     prompts: [
       {
         type: 'input',
         name: 'name',
-        message: 'What is your page name?'
+        message: 'What is your component name?'
       }
     ],
     actions: [
       {
         type: 'add',
-        path: '../src/app/{{dashCase name}}/page.js',
-        templateFile: 'templates/Page.js.hbs'
+        path: '../src/components/{{pascalCase name}}/index.jsx',
+        templateFile: 'templates/Component.jsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.js',
+        templateFile: 'templates/styles.js.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.jsx',
+        templateFile: 'templates/stories.jsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/test.jsx',
+        templateFile: 'templates/test.jsx.hbs'
       }
     ]
-  })
+  }),
+    plop.setGenerator('template', {
+      description: 'Create a template',
+      prompts: [
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is your template name?'
+        }
+      ],
+      actions: [
+        {
+          type: 'add',
+          path: '../src/templates/{{pascalCase name}}/index.jsx',
+          templateFile: 'templates/Template.jsx.hbs'
+        },
+        {
+          type: 'add',
+          path: '../src/templates/{{pascalCase name}}/styles.js',
+          templateFile: 'templates/styles.js.hbs'
+        },
+        {
+          type: 'add',
+          path: '../src/components/{{pascalCase name}}/test.jsx',
+          templateFile: 'templates/test.jsx.hbs'
+        }
+      ]
+    }),
+    plop.setGenerator('page', {
+      description: 'Create a page',
+      prompts: [
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is your page name?'
+        }
+      ],
+      actions: [
+        {
+          type: 'add',
+          path: '../src/app/{{dashCase name}}/page.js',
+          templateFile: 'templates/Page.js.hbs'
+        }
+      ]
+    })
 }
