55f5540bd28ad791f4e7b995c51df41eea7c9dc7
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2023-11-11 11:51:32 -0300

    add header component with styles and responsive rules

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { useState } from "react"
+import Image from "next/image"
+import Link from "next/link"
+import { Menu } from "../menu/Menu"
+import loggiLogo from "../../assets/loggi-logo-white.svg"
+import "./header.css"
+
+export const Header = () => {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => {
+    setOpen(!open)
+  }
+
+  return (
+    <header className="header">
+      <div className="header__grid">
+        <Link href="/">
+          <Image src={loggiLogo} alt="Logotipo Loggi" width={92} height={31} />
+        </Link>
+        <button
+          className="nav__button"
+          aria-label="abrir menu"
+          onClick={handleOpen}
+        >
+          Menu
+        </button>
+        <Menu open={open} />
+      </div>
+    </header>
+  )
+}
