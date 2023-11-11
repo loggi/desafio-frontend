e0aea5745ba156ff559078d19cfcda305ad92e83
André Beserra de Souza <andre.souza@cobasi.com.br>
2023-11-11 12:18:33 -0300

    feat(material-ui-nextjs): template configuration

---
+++ src/app/layout.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+// import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import DashboardIcon from '@mui/icons-material/Dashboard';
+import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
+
+export const metadata = {
+  title: 'Acompanhe suas Entregas em Tempo Real | Loggi',
+  description: 'Com a Loggi você consegue acompanhar sua entrega e saber exatamente quando seu pedido vai chegar. Acesse nosso rastreador e veja como é fácil!',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>
+        <ThemeRegistry>
+          {/* <AppBar position="fixed" sx={{ zIndex: 2000 }}>
+            <Toolbar sx={{ backgroundColor: 'background.paper' }}>
+              <DashboardIcon sx={{ color: '#444', mr: 2, transform: 'translateY(-2px)' }} />
+              <Typography variant="h6" noWrap component="div" color="black">
+                Next.js App Router
+              </Typography>
+            </Toolbar>
+          </AppBar> */}
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              bgcolor: 'background.default',
+              // mt: ['48px', '56px', '64px'],
+              p: 3,
+            }}
+          >
+            {children}
+          </Box>
+        </ThemeRegistry>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/ThemeRegistry/theme.js
@@ -0,0 +1,30 @@
+import { Montserrat } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+const montserrat = Montserrat({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+  typography: {
+    fontFamily: montserrat.style.fontFamily,
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === 'info' && {
+            backgroundColor: '#006aff',
+          }),
+        }),
+      },
+    },
+  },
+});
+
+export default theme;
