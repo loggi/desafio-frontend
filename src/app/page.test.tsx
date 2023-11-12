4fbe6cd9d7cbd67ccdfba205b3d84d26a6c7d38e
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2023-11-12 11:54:09 -0300

    add Home Page with styles and test

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,10 +1,72 @@
 /**
  * @jest-environment jsdom
  */
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Home from "./page"
+
+jest.mock("next/navigation", () => ({
+  useRouter() {
+    return {
+      prefetch: () => null,
+    }
+  },
+}))
 
 it("Page Home: render a title", () => {
   render(<Home />)
-  expect(screen.getByRole("heading")).toHaveTextContent("Jaiminho")
+  const headingText = screen.getByRole("heading", { level: 1 })
+  expect(headingText).toHaveTextContent(/acompanhe com a Loggi/i)
+})
+
+it("Page Home: Render a page component with a input and button", async () => {
+  render(<Home />)
+
+  const inputElement = screen.getByPlaceholderText(/primeiro, /i)
+  const buttonElement = screen.getByRole("button")
+
+  expect(inputElement).toBeInTheDocument()
+  expect(buttonElement).toBeInTheDocument()
+})
+
+it("Page Home: Render with a button is disabled", async () => {
+  render(<Home />)
+
+  const buttonElement = screen.getByRole("button")
+
+  expect(buttonElement).toBeInTheDocument()
+  expect(buttonElement).toBeDisabled()
+})
+
+it("Page Home: Type a invalid code and button is disabled", async () => {
+  render(<Home />)
+
+  const trackingCode = "046920318"
+
+  const inputElement = screen.getByPlaceholderText(/primeiro, /i)
+  const buttonElement = screen.getByRole("button")
+
+  expect(inputElement).toBeInTheDocument()
+  expect(buttonElement).toBeInTheDocument()
+
+  fireEvent.change(inputElement, { target: { value: trackingCode } })
+
+  expect(inputElement).toHaveValue("046920318")
+  expect(buttonElement).toBeDisabled()
+})
+
+it("Page Home: Type a valid code and button not is disabled", async () => {
+  render(<Home />)
+
+  const trackingCode = "TBR046920318"
+
+  const inputElement = screen.getByPlaceholderText(/primeiro, /i)
+  const buttonElement = screen.getByRole("button")
+
+  expect(inputElement).toBeInTheDocument()
+  expect(buttonElement).toBeInTheDocument()
+
+  fireEvent.change(inputElement, { target: { value: trackingCode } })
+
+  expect(inputElement).toHaveValue("TBR046920318")
+  expect(buttonElement).not.toBeDisabled()
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,50 @@
-import { LinkButton } from "@/components/linkButton/LinkButton"
+"use client"
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+import { Button } from "@/components/button/Button"
+import { Input } from "@/components/input/Input"
+import { Typography } from "@/components/typography/Typography"
 import styles from "./page.module.css"
 
 export default function Home() {
+  const router = useRouter()
+  const [trackCode, setTrackCode] = useState("")
+  const [isDisabled, setIsDisabled] = useState(true)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const regex = /^[A-Z]{3}\d{9}$/
+
+    setIsDisabled(true)
+
+    if (regex.test(event.target.value)) {
+      setTrackCode(event.target.value)
+      setIsDisabled(false)
+    }
+  }
+
+  const handleClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault()
+    router.push(`/rastrear/${trackCode}`)
+  }
   return (
-    <main className={styles.main}>
-      <h1>Jaiminho</h1>
-      <LinkButton variant="inverse" href="large">
-        botao
-      </LinkButton>
+    <main className={styles.home}>
+      <div className={styles.home__container}>
+        <Typography variant="h1">
+          Acompanhe com a Loggi a entrega do seu pedido.
+        </Typography>
+        <form className={styles.home__form}>
+          <Input
+            name="search"
+            id="search"
+            label="Código de rastreamento"
+            placeholder="Primeiro, digite o código de rastreamento."
+            onChange={handleChange}
+          />
+          <Button size="large" onClick={handleClick} disabled={isDisabled}>
+            Acompanhar pedido
+          </Button>
+        </form>
+      </div>
     </main>
   )
 }
