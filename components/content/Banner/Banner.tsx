8a1242a58c647730167955234909a89854554ea5
Thiago Melo <thiagojosedemelo@hotmail.com>
2023-12-09 17:39:46 -0300

    refactor: code improvement

--- components/content/Banner/Banner.tsx
+++ components/content/Banner/Banner.tsx
@@ -1,45 +1,61 @@
 'use client'
 
-import style from './Banner.module.sass';
-
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import RoomOutlined from '@mui/icons-material/RoomOutlined';
+
 import { useRouter } from 'next/navigation'
+import { useRef, useState } from 'react';
+
+import style from './Banner.module.sass';
 
 export const Banner = () => {
-
+    const [errorInput, setErrorInput] = useState<Boolean|any>(false)
     const router = useRouter();
+    const inputCodeReference = useRef<any>();
+
+    const onTracker = () => { 
+        const inputValue = inputCodeReference.current.value;
+        !inputValue
+            ? setErrorInput(true)
+            : setErrorInput(false)
+
+        inputValue && router.push(`/rastreador/${inputValue}`)
+    }
 
     return (
         <section className = {style.banner}>
             <div className="grid">
                 <Card 
-                    className={style['mb-1']}
                     variant="outlined" 
-                    sx={{ maxWidth: '50%' }}>
+                    sx={{ maxWidth: '30%', mb: 2 }}
+                >
                     <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
+                        <Typography variant="h5" color="text.secondary" sx={{ mb: 2 }}>
                             Acompanhe com a Loggi a entrega do seu pedido. 
                         </Typography>
-                        <label className={style.box__label} htmlFor="input-code">Código de rastreamento</label>
-                        <input 
-                            autoFocus
-                            className={style.input} 
-                            id="input-code"
-                            placeholder='Digite o código de rastreamento aqui...'
-                            type="text" 
+                        <TextField
+                            error={errorInput}
+                            inputRef={inputCodeReference} 
+                            label="Código de rastreamento" 
+                            variant="outlined"
+                            helperText={errorInput && "Precisamos do código de rastreamento."}
+                            sx={{ width: '100%', mb: 1 }}
                         />
-                        <button className={style.button} onClick={() => router.push('/rastreador')} >
-                            <FontAwesomeIcon icon={faLocationDot} className={style['button--icon']}/>
-                            Acompanhar pedido
-                        </button>
+                        <Button 
+                            variant="contained" 
+                            startIcon={<RoomOutlined />} 
+                            size="large"
+                            onClick={onTracker}
+                            sx={{width: '100%'}}>
+                                Acompanhar pedido
+                            </Button>
                     </CardContent>
                 </Card>
-
             </div>
         </section>
     )
