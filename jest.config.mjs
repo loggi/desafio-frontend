464d020159f7ee6527ffe9b56962e1609cd22dbf
Thiago Melo <thiagojosedemelo@hotmail.com>
2023-12-10 12:38:29 -0300

    feat: adding jest configuration

---
+++ jest.config.mjs
@@ -0,0 +1,14 @@
+import nextJest from 'next/jest.js'
+ 
+const createJestConfig = nextJest({
+  dir: './',
+})
+ 
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+ 
+export default createJestConfig(config)
